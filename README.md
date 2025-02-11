2057d595ab81b6cac0beb611c1f33d65f1350928
vishnusidhan <vsidhan@gmail.com>
2025-02-11 09:56:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_qr_generation_example_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_qr_generation_example_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_qr_generation_example_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
